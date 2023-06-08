a3cabaea70c4b1e37c52492ba2a5ed98352f0224
lalithagit6766 <109226579+lalithagit6766@users.noreply.github.com>
2023-06-08 15:21:56 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_691af26baeec7aaf3ca2e54ac51fbd974df65424',
+	    'sonar.login': 'squ_6b12b32808f87f0b7c84e2f40820b6cf7f1839de',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
